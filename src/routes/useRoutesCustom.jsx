c7c76b5046f5ae54ff38db33308a756d265ae488
PhamjDangwjAnhVuongw <phamvuong8199@gmail.com>
2024-03-16 14:41:54 +0700

    Source initial

---
+++ src/routes/useRoutesCustom.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useRoutes } from "react-router-dom";
+
+const useRoutesCustom = () => {
+  const routes = useRoutes();
+  return routes;
+};
+
+export default useRoutesCustom;
